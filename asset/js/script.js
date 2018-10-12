9217935dda5a8e7e1ba9ec2692a828406ec9a760
Cristian <00086917@uca.edu.sv>
2018-10-12 08:36:33 -0600

    avanzando

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,2 +1,23 @@
 var container;
 
+container = document.getElementById("c1");
+
+container.innerText = "Hola Mundo!";
+
+container.style.backgroundColor = "#000000";
+
+container.style.color = "#1cb723";
+
+container.style.width = "200px";
+
+container.style.height = "200px";
+
+container.innerHTML = "<input type=”text” placeholder=’escriba su texto’>";
+
+var containers;
+
+/*------------------------------------*/
+
+
+container = document.getElementsByClassName("c2");
+
